74bb765c126394f5fcce1bc218a2b73a294b49e4
muthu.mac <kumarmuthu.m12@gmail.com>
2024-07-15 18:52:53 +0400

    sg module under testing

--- SG/main.tf
+++ SG/main.tf
@@ -1,3 +1,33 @@
-resource "aws_security_group_rule" "sg-rule" {
-  
+
+locals {
+  valid_sg_rules_info = { for k,v in var.var.sg-rules : k=>v 
+  if v.port != "" && v.cidr_block != ""  }
+}
+
+resource "aws_security_group" "sg" {
+  vpc_id = var.vpc-id
+
+  dynamic "ingress" {
+    for_each = local.valid_sg_rules_info
+    content {
+      from_port = each.value.port
+      to_port = each.value.port
+      protocol = each.value.protocol
+      cidr_blocks = each.value.cidr_block
+    }
+  }
+
+  dynamic "egress" {
+    for_each = local.valid_sg_rules_info
+    content {
+      from_port = each.value.port
+      to_port = each.value.port
+      protocol = each.value.protocol
+      cidr_blocks = each.value.cidr_block
+    }
+  }
+
+  tags = {
+    Name = "${var.tag_name}-sg"
+  }
 }
--- TEST/test.tf
+++
@@ -1,8 +0,0 @@
-module "vpc" {
-  source = "../VPC"
-
-  tag_name = "mk-lab"
-  cidr_block = "10.0.0.0/16"
-  public_subnets = [ "10.0.1.0/24" ]
-  azs = "us-east-1a"
-}
