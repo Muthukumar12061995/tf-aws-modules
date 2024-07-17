ef625a50a37c89ed76454af3d0eac040d608f7b7
muthu.mac <kumarmuthu.m12@gmail.com>
2024-07-17 21:11:39 +0400

    EKS module added need to test

---
+++ EKS/controlNode.tf
@@ -0,0 +1,42 @@
+
+# Cluster IAM role
+resource "aws_iam_role" "cluster_role" {
+  name = "${var.env}-cluster-role"
+  assume_role_policy = jsonencode(
+    {
+      Version : "2012-10-17"
+      Statement: [
+        {
+          Action : "std:AssumeRole"
+          Effect : "Allow"
+          Pricipal : {
+            Service : "ec2.amazonaws.com"
+          }
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  role = aws_iam_role.cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  role = aws_iam_role.cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_eks_cluster" "controlNode" {
+  name = var.cluster_name
+  role_arn = aws_iam_role.cluster_role.arn
+  version = var.cluster_version
+
+  vpc_config {
+     subnet_ids = var.cluster_subnet_ids
+  }
+
+  depends_on = [ aws_iam_role.cluster_role ]
+}
+
--- EKS/variables.tf
+++ EKS/variables.tf
@@ -1,4 +1,30 @@
+variable "env" {type = string}
 variable "cluster_name" {type = string}
 variable "cluster_version" {type = string}
-variable "cluster_role_arn" {type = string}
-variable "cluster_subnet_ids" {type = string}
+variable "cluster_subnet_ids" {
+  type = list(string)
+  default = [""]
+}
+
+variable "worker-node-info" {
+    type = map(object({
+      node_group_name = string
+      instance_types = list(string)
+      subnet_ids = list(string)
+      desired_size = number
+      min_size = number
+      max_size = number
+    }))
+
+    default = {
+      "default-worker-node" = {
+        node_group_name = ""
+        instance_types = ""
+        subnet_ids = ""
+        desired_size = 0
+        min_size = 0
+        max_size = 0
+      }
+    }
+  
+}
